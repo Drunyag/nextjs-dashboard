51e08281f982dc7228d90c49699f9653e5f91028
andrey <drugmee@gmail.com>
2024-11-26 16:18:18 +0100

    finished

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import {z} from 'zod';
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
 import {ensureFormDataField} from "@/app/lib/utilesJs";
+import {signIn} from "@/auth";
+import {AuthError} from "next-auth";
 
 
 const FormSchema = z.object({
@@ -142,4 +144,23 @@ export async function deleteInvoice(id: string) {
         };
     }
 
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
